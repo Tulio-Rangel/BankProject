2dd06538019c60c5676ef89314574a4dd9c2d4be
Tulio-Rangel <tjrn20@hotmail.com>
2025-01-09 14:58:55 -0500

    refactor: decouple user controller

--- src/test/java/com/tulio/banksofka/controller/UserControllerTest.java
+++ src/test/java/com/tulio/banksofka/controller/UserControllerTest.java
@@ -1,9 +1,10 @@
 package com.tulio.banksofka.controller;
 
-import com.tulio.banksofka.dto.BalanceDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tulio.banksofka.model.BankAccount;
+import com.tulio.banksofka.model.User;
 import com.tulio.banksofka.security.JwtAuthorizationFilter;
 import com.tulio.banksofka.security.SecurityConfigTest;
-import com.tulio.banksofka.service.AccountService;
 import com.tulio.banksofka.service.UserService;
 import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,9 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
@@ -23,15 +27,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(BankController.class)
+@WebMvcTest(UserController.class)
 @Import(SecurityConfigTest.class)
-class BankControllerTest {
+public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @MockBean
     private UserService userService;
-    @MockBean
-    private AccountService accountService;
     @MockBean
     private JwtAuthorizationFilter jwtAuthorizationFilter;
 
@@ -41,24 +43,35 @@ class BankControllerTest {
     }
 
     @Test
-    void getBalance_ShouldReturnBalanceInfo() throws Exception {
+    void createUser_ShouldReturnCreatedUser() throws Exception {
         // Arrange
-        BalanceDTO balanceDTO = new BalanceDTO("1234567890", 100.0);
-        when(accountService.getBalanceInfo(1L)).thenReturn(balanceDTO);
+        User user = new User();
+        user.setEmail("test@test.com");
+        user.setPassword("password");
+        user.setName("Test User");
+
+
+        when(userService.createUser(any(User.class))).thenReturn(user);
+        doNothing().when(jwtAuthorizationFilter).doFilter(any(), any(), any());
 
         // Act & Assert
-        mockMvc.perform(get("/api/accounts/1/balance"))
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(user)))
                 .andExpect(status().isOk());
     }
 
     @Test
-    void makeDeposit_ShouldReturnOk() throws Exception {
+    void getUserAccounts_ShouldReturnUserAccounts() throws Exception {
         // Arrange
-        doNothing().when(accountService).makeDeposit(1L, 100.0);
+        User user = new User();
+        List<BankAccount> accounts = Arrays.asList(new BankAccount(), new BankAccount());
+        user.setAccounts(accounts);
+
+        when(userService.findById(1L)).thenReturn(user);
 
         // Act & Assert
-        mockMvc.perform(post("/api/accounts/1/deposit")
-                        .param("amount", "100.0"))
+        mockMvc.perform(get("/api/users/1/accounts"))
                 .andExpect(status().isOk());
     }
 }
