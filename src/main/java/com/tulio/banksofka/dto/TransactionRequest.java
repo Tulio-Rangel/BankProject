9f033aaa58265f90fa61873421573839b5dd39af
Tulio-Rangel <tjrn20@hotmail.com>
2025-01-27 17:13:14 -0500

    feat: change requestParam for RequestBody for deposit&withdrawal

---
+++ src/main/java/com/tulio/banksofka/dto/TransactionRequest.java
@@ -0,0 +1,13 @@
+package com.tulio.banksofka.dto;
+
+public class TransactionRequest {
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
