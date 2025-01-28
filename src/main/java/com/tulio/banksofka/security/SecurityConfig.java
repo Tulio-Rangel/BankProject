b9e8bdd58a09797e13e9beab8640c6a917798e0e
Tulio-Rangel <tjrn20@hotmail.com>
2025-01-28 08:57:46 -0500

    add host for angular in allowed origins

--- src/main/java/com/tulio/banksofka/security/SecurityConfig.java
+++ src/main/java/com/tulio/banksofka/security/SecurityConfig.java
@@ -78,6 +78,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
         configuration.setExposedHeaders(List.of("Authorization"));
